b51e42ef557010dbb9cd30309092976268cd3a1a
abhishekabhiraj <bcoder48@gmail.com>
2025-05-10 18:27:59 +0530

    app initialised

---
+++ driver/components/Categories.jsx
@@ -0,0 +1,52 @@
+import { FlashList } from '@shopify/flash-list'
+import { Link } from 'expo-router'
+import React from 'react'
+import { Image, Pressable, Text, View } from 'react-native'
+
+import FeedSectionContainer from './common/FeedSectionContainer'
+
+export default function Categories(props) {
+  //? Props
+  const { childCategories, color, name } = props;
+
+
+  //? Re-Renders
+  if (childCategories.categories.length > 0) {
+    return (
+      <FeedSectionContainer title="Categorize">
+        <FlashList
+          data={childCategories.categories}
+          horizontal
+          renderItem={({ item, index }) => (
+            <Link
+              key={item._id}
+              href={{
+                pathname: '/products',
+                params: { category: item.slug },
+              }}
+              asChild
+            >
+              <Pressable className="flex items-center mr-3 space-y-2">
+                <View className="w-14 h-14 rounded-full border-solid border-2 border-slate-200 overflow-hidden">
+                  <Image
+                    key={index}
+                    source={{
+                      uri: item.image,
+                    }}
+                    className="w-full h-full"
+                  />
+                </View>
+                <Text className="text-gray-700">
+                {/* {item.name} */}
+                Taxi
+                </Text>
+              </Pressable>
+            </Link>
+          )}
+          estimatedItemSize={400}
+        />
+      </FeedSectionContainer>
+    )
+  }
+  return null
+}
---
+++ driver/utils/constatns.js
@@ -0,0 +1,25 @@
+export const sorts = [
+  {
+    name: 'Latest',
+    value: 1,
+  },
+  {
+    name: 'Best Selling',
+    value: 2,
+  },
+  {
+    name: 'Cheapest',
+    value: 3,
+  },
+  {
+    name: 'Hottest',
+    value: 4,
+  },
+]
+
+export const ratingStatus = ['', 'Worst', 'Bad', 'Average', 'Good', 'Excellent']
+
+export const siteTitle = 'Featured Deals'
+export const enSiteTitle = 'C-Shopping'
+
+export const siteDescription = `Buy everything you need at the best prices from ${siteTitle}! Latest phones, laptops, clothes, cosmetics, books, appliances, cars, and more. Easy returns | ✓Free delivery ✓Cash on delivery ✓Return guarantee - Click to shop now!`
